fad5e2fa4db212f957ab4b782847c919317b8f89
JiaweiGit1999 <101209798@students.swinburne.edu.my>
2020-10-25 19:40:28 +0800

    refactor code to jquery

--- Web Application/residents.js
+++ Web Application/residents.js
@@ -1,27 +1,21 @@
-
-var resident_list = document.getElementById("list");
-var list = "";
-db.collection("landlord").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-		console.log(doc.data().name);
-		var user =`<tr><td>${doc.data().name}</td>
-				<td>${doc.data().email}</td>
-				<td>${doc.data().contact}</td>
-				<td>${doc.data().ic}</td>
-				<td>${doc.data().unit}</td>
-				<td>${doc.data().role}</td>
-				<td><button>Delete</button></td>
-		</tr>`
-		
-		list = list.concat(user);
-		
-    });
-	
-	resident_list.innerHTML = list;
-}).then(()=>{
-	$('#resident-list').DataTable({
+var t = $('#resident-list').DataTable({
 					"pagingType": "simple_numbers",
 					info: false,
 					"sDom": '<"top"i>rt<"bottom"flp><"clear">'
 				});	
+db.collection("landlord").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+		console.log(doc.data().name);
+		
+		t.row.add( [
+            doc.data().name,
+            doc.data().email,
+            doc.data().contact,
+            doc.data().ic,
+            doc.data().unit,
+			doc.data().role,
+			"<button>Delete</button>"
+        ] ).draw( );
+    });
+	
 });
