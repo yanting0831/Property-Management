66e6f3d9d4e4463052970d3bbd0831a7f02f3580
JiaweiGit1999 <101209798@students.swinburne.edu.my>
2021-03-31 10:13:11 +0800

    added booking limit

--- Web Application/booking-settings.js
+++ Web Application/booking-settings.js
@@ -3,6 +3,54 @@ $(function() {
     opens: 'left'
   }, function(start, end, label) {
     console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+	
+	var submit_btn = document.getElementById("complete-button");
+
+	submit_btn.addEventListener("click",async function(e){
+		e.preventDefault();
+		
+		var limit = document.getElementById("limit").value;
+		var facility = document.getElementById("facility").value;
+		
+		var timeslots = document.getElementsByClassName("timeslots");
+		var timeslot_checked = [];
+		
+		for(var i = 0; i<timeslots.length; i++){
+			if(timeslots[i].checked == true){
+				timeslot_checked.push(timeslots[i].value);
+			}
+		}
+		
+		
+		var start_date = start.format('MM-DD-YYYY');
+		var end_date = end.format('MM-DD-YYYY');
+		
+		var now = new Date();
+		var daysOfYear = [];
+		var batch = db.batch();
+		var disabled_dateRef = db.collection("disabled_dates");
+		
+		
+		for (var d = new Date(start_date); d <= new Date(end_date); d.setDate(d.getDate() + 1)) {
+			
+			var month = d.getMonth();
+			var day = String(d.getDate()).padStart(2, '0');
+			var year = d.getFullYear();
+			var date = day  + '-'+ month  + '-' + year;
+			console.log("preparing docs");
+			doc = {
+				date: date,
+				disabled_time: timeslot_checked,
+				facility:"AV Room",
+				limit: limit
+			}
+				
+			await disabled_dateRef.add(doc);
+			
+			console.log("doc added");
+		}
+	})
+	
   });
 });
 
@@ -17,3 +65,4 @@ selectall.addEventListener("click",function(e){
 		timeslots[i].checked = true;
 	}
 });
+
