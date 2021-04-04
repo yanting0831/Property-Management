bdfc582165dfb56e70d7b48b45cdb8ecaec2c1c5
JasonThian <100086673@students.swinburne.edu.my>
2021-04-04 14:48:01 +0800

    issue report done

--- Web Application/issue-report.js
+++ Web Application/issue-report.js
@@ -5,27 +5,52 @@ var t = $('#issue-report').DataTable({
 	"sDom": '<"top"i>rt<"bottom"flp><"clear">'
 });
 
-db.collection("issues").get().then((querySnapshot) => {
-	querySnapshot.forEach((doc) => {
-		var img_path = "issues/" + doc.data().img;
-		storage.ref().child(img_path).getDownloadURL().then(function(url) {
-			var image_link = '<button class="image_link" type="button" onclick="show_img(\''+url+'\');">Show</button>';
-			t.row.add([
-				doc.data().date.toDate().toLocaleString(),
-				doc.data().name,
-				doc.data().pno,
-				doc.data().email,
-				doc.data().block,
-				image_link,
-				doc.data().desc
-			]).draw();
-		}).catch((err) => {
-			console.log(err);  
+var issuesDoc = db.collection("issues");
+
+function get_data(){
+	issuesDoc.get().then((querySnapshot) => {
+		querySnapshot.forEach((doc) => {
+			var img_path = "issues/" + doc.data().img;
+			var issue_status = "Not Solved"; 
+			var action_button = '<button type="button" onclick="solve(\''+doc.id+'\');">Mark as Solved</button>';
+			
+			if(doc.data().status != undefined)
+				issue_status = doc.data().status;
+			
+			if(issue_status == "Solved")
+				action_button = '<button type="button" onclick="unsolve(\''+doc.id+'\');">Mark as Unsolved</button>';
+			
+			storage.ref().child(img_path).getDownloadURL().then(function(url) {
+				var image_link = '<button class="image_link" type="button" onclick="show_img(\''+url+'\');">Show</button>';
+				t.row.add([
+					doc.data().date.toDate().toLocaleString(),
+					doc.data().name,
+					doc.data().pno,
+					doc.data().email,
+					doc.data().block,
+					image_link,
+					doc.data().desc,
+					issue_status,
+					action_button
+				]).draw();
+			}).catch((err) => {
+				t.row.add([
+					doc.data().date.toDate().toLocaleString(),
+					doc.data().name,
+					doc.data().pno,
+					doc.data().email,
+					doc.data().block,
+					"Image not found",
+					doc.data().desc,
+					issue_status,
+					action_button
+				]).draw();
+			});
 		});
+	}).catch((err) => {
+		console.log(err);  
 	});
-}).catch((err) => {
-	console.log(err);  
-});
+}
 
 function show_img(url){
 	var img_container = document.getElementById("img-container");
@@ -38,3 +63,19 @@ function hide(){
 	var img_container = document.getElementById("img-container");
 	img_container.style.display = "None";
 }
+
+function solve(id){
+	issuesDoc.doc(id).update({ status: "Solved" }).then(() => {
+		t.clear();
+		get_data();
+	});
+}
+
+function unsolve(id){
+	issuesDoc.doc(id).update({ status: "Not Solved" }).then(() => {
+		t.clear();
+		get_data();
+	});
+}
+
+get_data();
