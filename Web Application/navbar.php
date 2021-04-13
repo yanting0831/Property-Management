a8646cc6cbdbe9ef807854fc451698e8e7cbe5f2
Eric1231 <101208805@students.swinburne.edu.my>
2021-04-13 14:34:47 +0800

    Update navbar.php

--- Web Application/navbar.php
+++ Web Application/navbar.php
@@ -4,14 +4,16 @@
 	<div class="sidebar">
 		<header><img src="images/dryx-black.png" alt="dryx-logo" width="50%"></header>
 			<ul>
-				<li><a href="residents.html"><i class="fas fa-user-friends"></i>Residents</a></li>
-				<li><a href="livechat.html"><i class="fas fa-comment-dots"></i>Live Chat</a></li>
-				<li><a href="update-bill.html"><i class="fas fa-bolt"></i>Update bill</a></li>
-				<li><a href="payment-log.html"><i class="fas fa-money-bill-alt"></i>Payment Log</a></li>
-				<li><a href="visitor-log.html"><i class="fas fa-address-card"></i>Visitor Log</a></li>
-				<li><a href="announcement.html"><i class="fas fa-bell"></i>Announcements</a></li>
-				<li><a href="bookings.html"><i class="fas fa-building"></i>Facility bookings</a></li>
-				<li><a href="login.html" id="logout"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
+				<li><a href="residents.php"><i class="fas fa-user-friends"></i>Residents</a></li>
+				<li><a href="livechat.php"><i class="fas fa-comment-dots"></i>Live Chat</a></li>
+				<li><a href="update-bill.php"><i class="fas fa-bolt"></i>Update Bill</a></li>
+				<li><a href="payment-log.php"><i class="fas fa-money-bill-alt"></i>Payment Log</a></li>
+				<li><a href="visitor-log.php"><i class="fas fa-address-card"></i>Visitor Log</a></li>
+				<li><a href="Cannouncement.php"><i class="fas fa-bell"></i>Announcements</a></li>
+				<li><a href="bookings.php"><i class="fas fa-building"></i>Facility Bookings</a></li>
+				<li><a href="facility.php"><i class="fas fa-building"></i>Add Facility</a></li>
+				<li><a href="login.php" id="logout"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
+				
 			</ul>
 	</div>';
 ?>
