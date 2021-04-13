b576c17489157ec8f14f79bb0a0ae672566e52dc
Eric1231 <101208805@students.swinburne.edu.my>
2021-04-13 13:41:17 +0800

    facility

--- Web Application/facility.js
+++ Web Application/facility.js
@@ -268,3 +268,70 @@ function createGraph(){
 $(document).ready(() => {
 	createGraph();
 });
+
+
+var t = $('#resident-list').DataTable({
+					"pagingType": "simple_numbers",
+					info: false,
+					"sDom": '<"top"i>rt<"bottom"flp><"clear">'
+				});	
+db.collection("facility").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+		var button = '';
+		console.log(button);
+		t.row.add( [
+            doc.data().title,
+			`<button id='${doc.id}'type="button" class='deleteuser'>Delete</button>`,
+			button
+        ] ).draw( );
+    });
+	
+}).then(()=>{
+	var button_list = document.getElementsByClassName("deleteuser");
+	for (var i=0; i< button_list.length; i++ ) {
+		
+	 	button_list[i].addEventListener("click", async function(){
+			deleteUser(this.id)
+
+		});
+	}
+	
+});
+
+async function deleteUser(ele_id){
+	console.log(ele_id);
+	const deleteUser = functions.httpsCallable('deleteUser');
+	await deleteUser({ uid: ele_id }).then(result => {
+
+		if(result.data.message == "Success"){
+			console.log("user has been deleted");
+			deleteRecords(ele_id)
+		}else{
+			console.log(result);
+			deleteRecords(ele_id)
+			alert(result);
+		}
+	});
+}
+
+async function deleteRecords(id){
+	var p1 = db.collection("facility").doc(id).delete().then(function() {
+		console.log("Document successfully deleted!");
+	}).catch(function(error) {
+		console.error("Error removing document: ", error);
+	});
+	
+	await p1.then(()=>{
+		var storageRef = storage.ref();
+		var imageRef = storageRef.child(id+'.png');
+
+		// Delete the file
+		imageRef.delete().then(function() {
+		  console.log("image successfully deleted!");
+		}).catch(function(error) {
+		  console.error("Error removing document: ", error);
+		});
+	}).then(()=>{
+		location.reload();
+	});
+}
