f35ecb03003138bd8f8cd232e4cd8fa35aaadf8d
Yan Ting <101209183@students.swinburne.edu.my>
2021-04-17 17:14:55 +0800

    Update auth(logged in).js

--- Web Application/auth(logged in).js
+++ Web Application/auth(logged in).js
@@ -1,31 +1,31 @@
 // listen for auth status changes
-auth.onAuthStateChanged(user => {
-  if (!user) {
+// auth.onAuthStateChanged(user => {
+//   if (!user) {
 	  
-	  console.log("no user logged in.");
-	  location.replace("login.html");
-  }else{
-	  console.log(user,"logged in.");
-	  user.getIdTokenResult().then(idTokenResult => {
-		user.master = idTokenResult.claims.master;
-		user.admin = idTokenResult.claims.admin;
-			if (!user.master && !user.admin) {
-				console.log("user is not a master or admin");
-				logout();	
-			}
-    	});
-  }
-});
+// 	  console.log("no user logged in.");
+// 	  location.replace("login.php");
+//   }else{
+// 	  console.log(user,"logged in.");
+// 	  user.getIdTokenResult().then(idTokenResult => {
+// 		user.master = idTokenResult.claims.master;
+// 		user.admin = idTokenResult.claims.admin;
+// 			if (!user.master && !user.admin) {
+// 				console.log("user is not a master or admin");
+// 				logout();	
+// 			}
+//     	});
+//   }
+// });
 //logout listener
-document.getElementById("logout").addEventListener("click",logout);
+// document.getElementById("logout").addEventListener("click",logout);
 
-console.log("Initializing auth script");
+// console.log("Initializing auth script");
 
-function logout(){
+// function logout(){
 	
 	
-	auth.signOut().then(()=>{
-		console.log("user signed out");
-		location.replace("login.html");
-	});
-}
+// 	auth.signOut().then(()=>{
+// 		console.log("user signed out");
+// 		location.replace("login.php");
+// 	});
+// }
