18480e939fdb53afabf87bed24b5623e5ee546de
Yan Ting <101209183@students.swinburne.edu.my>
2021-04-17 17:15:03 +0800

    Update navbar.php

--- Web Application/navbar.php
+++ Web Application/navbar.php
@@ -2,14 +2,14 @@
 	echo '	
 	
 	<div class="sidebar">
-		<header><img src="images/dryx-black.png" alt="dryx-logo" width="50%"></header>
 			<ul>
+				<img src="images/dryx-logo.png" alt="dryx-logo" width="50%">
 				<li><a href="residents.php"><i class="fas fa-user-friends"></i>Residents</a></li>
 				<li><a href="livechat.php"><i class="fas fa-comment-dots"></i>Live Chat</a></li>
 				<li><a href="update-bill.php"><i class="fas fa-bolt"></i>Update Bill</a></li>
 				<li><a href="payment-log.php"><i class="fas fa-money-bill-alt"></i>Payment Log</a></li>
 				<li><a href="visitor-log.php"><i class="fas fa-address-card"></i>Visitor Log</a></li>
-				<li><a href="Cannouncement.php"><i class="fas fa-bell"></i>Announcements</a></li>
+				<li><a href="announcement.php"><i class="fas fa-bell"></i>Announcements</a></li>
 				<li><a href="bookings.php"><i class="fas fa-building"></i>Facility Bookings</a></li>
 				<li><a href="facility.php"><i class="fas fa-building"></i>Add Facility</a></li>
 				<li><a href="login.php" id="logout"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
